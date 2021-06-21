a08b8268ee7f07acb3bcbfb1cbd9bc90ae7f6ed0
Emanlui <emanuellejs1999@gmail.com>
2021-06-20 22:11:28 -0400

    Yara rules added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # E-Zeus
 This tool analyze executables and APK files
+
+
+
+
+## References
+
+https://github.com/VectraThreatLab/reyara/blob/master/re.yar
+https://github.com/reversinglabs/reversinglabs-yara-rules
+https://github.com/InQuest/yara-rules
+https://github.com/intezer/yara-rules
+https://github.com/fboldewin/YARA-rules
+https://github.com/f0wl/yara_rules
--- e-zeus.py
+++ e-zeus.py
@@ -3,6 +3,33 @@
 import argparse
 import os
 import sys
+import puremagic as magic
+from colorama import Fore, Style, init
+import yara
+from androguard.core.bytecodes.apk import APK
+
+
+def yaraAnalysis(file_to_analyze):
+
+	onlyfiles = []
+
+	for dirpath, dirs, files in os.walk("YaraRules/"):  
+		for filename in files:
+
+			if(".yar" in filename or ".rule" in filename):
+				onlyfiles.append(os.path.join(dirpath,filename))
+			
+
+	match = ""
+
+	for i in onlyfiles:
+		rules = yara.compile(i)
+		temp_match = rules.match(file_to_analyze)
+		if temp_match != []:
+			for i in temp_match:
+				match =	match + "\n" + str(i)
+		
+	print(match)
 
 def setupParser():
 	args = []
@@ -29,6 +56,17 @@ def setupParser():
 	args = parser.parse_args()
 	return args 
 
+def analyze(file_to_analyze):
+
+	type_of_the_file = str(magic.magic_file(file_to_analyze))
+
+	# Windows Analysis
+	if "Windows Executable" in type_of_the_file or ".msi" in type_of_the_file or ".dll" in type_of_the_file or ".exe" in type_of_the_file or ".drv" in type_of_the_file or ".sdb" in type_of_the_file or ".sys" in type_of_the_file or ".reg" in type_of_the_file:
+		print(Fore.GREEN + '--- Analyzing Windows executable ---'+Fore.WHITE)
+		yaraAnalysis(file_to_analyze)
+		
+	elif ".xltx" in type_of_the_file or ".xlam" in type_of_the_file or ".docm" in type_of_the_file or ".dotx" in type_of_the_file or ".pptm" in type_of_the_file or ".xlsm" in type_of_the_file or ".ppt" in type_of_the_file or ".doc" in type_of_the_file or ".xla" in type_of_the_file:
+		print(Fore.GREEN + 'Analyzing Windows document...'+Fore.WHITE)		
 
 def zeus(args):
 
@@ -38,6 +76,7 @@ def zeus(args):
 	elif(args.analyze):
 		print("analyze")
 		print(args.analyze)
+		analyze(args.analyze)
 	elif(args.multifile):
 		print("multifile")
 	elif(args.docs):
